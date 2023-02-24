79ff966f52b772cdcf266d6379d54968f783fcd8
Will <whenchyiv@users.noreply.github.com>
2023-02-24 12:36:57 -0500

    Fix validation for ModelSelectMultipleField

--- flask_admin/form/validators.py
+++ flask_admin/form/validators.py
@@ -2,6 +2,7 @@ from flask_admin.babel import gettext
 
 from wtforms.validators import StopValidation
 from wtforms.fields import SelectMultipleField
+from flask_mongoengine.wtf.fields import ModelSelectMultipleField
 
 
 class FieldListInputRequired(object):
@@ -12,11 +13,18 @@ class FieldListInputRequired(object):
     field_flags = ('required',)
 
     def __call__(self, form, field):
-        if type(field) is not SelectMultipleField:
-            if len(field.entries) == 0:
+        if type(field) is ModelSelectMultipleField:
+            print(field.data)
+            if len(field.data) == 0:
+                field.errors[:] = []
+                raise StopValidation(gettext('This field requires at least one item.'))
+        elif type(field) is SelectMultipleField:
+            print('SELECTFIELD')
+            print(field.data)
+            if len(field.data) == 0:
                 field.errors[:] = []
                 raise StopValidation(gettext('This field requires at least one item.'))
         else:
-            if len(field.choices) == 0:
+            if len(field.entries) == 0:
                 field.errors[:] = []
                 raise StopValidation(gettext('This field requires at least one item.'))
