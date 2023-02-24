e869c1197ffbdbf983b5d9fa1b40511170418e6a
Will <whenchyiv@users.noreply.github.com>
2023-02-24 12:11:30 -0500

    Fix issue with SelectMultipleField and validation

--- flask_admin/form/validators.py
+++ flask_admin/form/validators.py
@@ -1,6 +1,7 @@
 from flask_admin.babel import gettext
 
 from wtforms.validators import StopValidation
+from wtforms.fields import SelectMultipleField
 
 
 class FieldListInputRequired(object):
@@ -11,6 +12,11 @@ class FieldListInputRequired(object):
     field_flags = ('required',)
 
     def __call__(self, form, field):
-        if len(field.entries) == 0:
-            field.errors[:] = []
-            raise StopValidation(gettext('This field requires at least one item.'))
+        if type(field) is not SelectMultipleField:
+            if len(field.entries) == 0:
+                field.errors[:] = []
+                raise StopValidation(gettext('This field requires at least one item.'))
+        else:
+            if len(field.choices) == 0:
+                field.errors[:] = []
+                raise StopValidation(gettext('This field requires at least one item.'))
